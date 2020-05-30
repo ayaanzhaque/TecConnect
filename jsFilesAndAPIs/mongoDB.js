3ef45e412c0eae16ac9037e5fd79aef7a3d8aa5c
ayaanzhaque <ayaanzhaque@gmail.com>
2020-05-30 11:49:31 -0700

    fixed warm feeded algorithm with mongoDB

--- jsFilesAndAPIs/mongoDB.js
+++ jsFilesAndAPIs/mongoDB.js
@@ -93,6 +93,44 @@ client.connect(function(err) {
   });
 });
 
+const Algorithn = algorithm(db, callback, school1, deviceCount1, location1, school2, deviceCount2, location2) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+
+  function distance(lat1, lon1, lat2, lon2, unit) {
+  	if ((lat1 == lat2) && (lon1 == lon2)) {
+  		return 0;
+  	}
+  	else {
+  		var radlat1 = Math.PI * lat1/180;
+  		var radlat2 = Math.PI * lat2/180;
+  		var theta = lon1-lon2;
+  		var radtheta = Math.PI * theta/180;
+  		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  		if (dist > 1) {
+  			dist = 1;
+  		}
+  		dist = Math.acos(dist);
+  		dist = dist * 180/Math.PI;
+  		dist = dist * 60 * 1.1515;
+  		if (unit=="K") { dist = dist * 1.609344 }
+  		if (unit=="N") { dist = dist * 0.8684 }
+  		return dist;
+  	}
+  }
+
+  if(deviceCount1 =~ deviceCount2){
+    return pairing;
+  }
+}
+
 const findDocuments = function(db, callback) {
   // Get the documents collection
   const collection = db.collection('documents');
